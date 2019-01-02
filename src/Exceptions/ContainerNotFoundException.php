271964cf456a4bbc43cf126a93eea458a14f53f0
Toon Van den Bos <toon@whitecube.be>
2019-01-02 15:52:31 +0100

    Fixed ContainerNotFoundException since SourceInterface changes

--- src/Exceptions/ContainerNotFoundException.php
+++ src/Exceptions/ContainerNotFoundException.php
@@ -2,7 +2,8 @@
 
 namespace Whitecube\NovaPage\Exceptions;
 
-use \Exception;
+use Exception;
+use Whitecube\NovaPage\Sources\SourceInterface;
 
 class ContainerNotFoundException extends Exception
 {
@@ -15,8 +16,8 @@ class ContainerNotFoundException extends Exception
      * @param int $code
      * @param Exception $previous
      */
-    public function __construct($source, $identifier, $code = 0, Exception $previous = null) {
-        $message = 'Unable to load "' . $identifier . '" from source "' . $source . '".';
+    public function __construct(SourceInterface $source, $identifier, $code = 0, Exception $previous = null) {
+        $message = 'Unable to load "' . $identifier . '" from source "' . $source->getName() . '".';
         parent::__construct($message, $code, $previous);
     }
 
