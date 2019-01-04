4da038c1a566d20533c019ecc45d93e7f3220c9a
Toon Van den Bos <toon@whitecube.be>
2019-01-04 18:20:32 +0100

    Added ResourceIndexController pagination

--- src/Http/Controllers/ResourceIndexController.php
+++ src/Http/Controllers/ResourceIndexController.php
@@ -6,6 +6,8 @@ use Illuminate\Routing\Controller;
 use Whitecube\NovaPage\Pages\Manager;
 use Whitecube\NovaPage\Pages\Resource;
 use Laravel\Nova\Http\Requests\ResourceIndexRequest;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ResourceIndexController extends Controller
 {
@@ -13,22 +15,38 @@ class ResourceIndexController extends Controller
      * List the resources for administration.
      *
      * @param  \Laravel\Nova\Http\Requests\ResourceIndexRequest  $request
+     * @param  \Whitecube\NovaPage\Pages\Manager  $manager
      * @return \Illuminate\Http\Response
      */
     public function handle(ResourceIndexRequest $request, Manager $manager)
     {
-        $collection = $manager->queryIndex($request);
+        $paginator = $this->paginator($request, $manager);
 
         return response()->json([
             'label' => config('novapage.label'),
-            'resources' => $collection->values(),
-            'prev_page_url' => null,
-            'next_page_url' => null,
+            'resources' => $paginator->getCollection()->values(),
+            'prev_page_url' => $paginator->previousPageUrl(),
+            'next_page_url' => $paginator->nextPageUrl(),
             'softDeletes' => null,
         ]);
-
-        \Log::debug($response);
-
-        return $response;
     }
+
+    /**
+     * Get the paginator instance for the index request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\ResourceIndexRequest  $request
+     * @param  \Whitecube\NovaPage\Pages\Manager  $manager
+     * @return \Illuminate\Pagination\Paginator
+     */
+    protected function paginator(ResourceIndexRequest $request, Manager $manager)
+    {
+        $page = Paginator::resolveCurrentPage() ?: 1;
+
+        $items = $manager->queryIndex($request);
+
+        $perPage = $request->perPage ?? 25;
+
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page);
+    }
+
 }
