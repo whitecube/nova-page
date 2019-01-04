a282207b46427052f80c445f7b0c369858d5de8a
Toon Van den Bos <toon@whitecube.be>
2019-01-04 21:15:30 +0100

    Removed unused asset resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix')
 
 mix.setPublicPath('dist')
-   .js('resources/js/tool.js', 'js')
-   .sass('resources/sass/tool.scss', 'css')
+   // .js('resources/js/tool.js', 'js')
+   // .sass('resources/sass/tool.scss', 'css')
