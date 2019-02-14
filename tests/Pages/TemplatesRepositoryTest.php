98688f6dd447b71e397d98b74342c90f9153a941
Adrien Leloup <adrien@whitecube.be>
2019-02-14 10:59:41 +0100

    Added TemplatesRepository tests

--- tests/Pages/TemplatesRepositoryTest.php
+++ tests/Pages/TemplatesRepositoryTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Pages;
 
+use FakeTestApp\Nova\Templates\Test;
 use Orchestra\Testbench\TestCase;
+use Whitecube\NovaPage\Exceptions\TemplateNotFoundException;
 use Whitecube\NovaPage\Pages\TemplatesRepository;
 
 class TemplatesRepositoryTest extends TestCase {
@@ -31,6 +33,47 @@ class TemplatesRepositoryTest extends TestCase {
     {
         $instance = $this->app->make(TemplatesRepository::class);
         $this->assertEquals([], $instance->getPages());
+    }
+
+    /** @test */
+    public function can_register_templates_defined_with_routes()
+    {
+        \Route::get('/test', 'TestController@foo')->template(Test::class)->name('test');
+        $instance = $this->app->make(TemplatesRepository::class);
+        $instance->registerRouteTemplates();
+        $this->assertCount(1, $instance->getPages());
+    }
+
+    /** @test */
+    public function can_register_templates()
+    {
+        $instance = $this->app->make(TemplatesRepository::class);
+        $instance->register('route', 'test', Test::class);
+        $this->assertCount(1, $instance->getPages());
+        $this->assertEquals('FakeTestApp\Nova\Templates\Test', $instance->getPages()['route.test']);
+    }
+
+    /** @test */
+    public function can_load_a_templates_values()
+    {
+        $instance = $this->app->make(TemplatesRepository::class);
+        $instance->register('route', 'test', Test::class);
+
+        // Load twice so we cover every bit of code in the load method
+        $instance->load('route', 'test', false);
+        $instance->load('route', 'test', false);
+
+        $this->assertNotNull($instance->getLoaded('route', 'test'));
+        $this->assertNull($instance->getLoaded('route', 'foo'));
+    }
+
+    /** @test */
+    public function throws_an_exception_when_loading_a_template_that_does_not_exist()
+    {
+        $this->expectException(TemplateNotFoundException::class);
+        $instance = $this->app->make(TemplatesRepository::class);
+        $instance->register('route', 'test', Test::class);
+        $instance->load('route', 'foo', false);
     }
 
 }
