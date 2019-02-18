1072b53854fee1d00495d2897681ef886ef391ec
Adrien Leloup <adrien@whitecube.be>
2019-02-18 10:36:30 +0100

    Added Filesystem tests

--- tests/Unit/Sources/FilesystemTest.php
+++ tests/Unit/Sources/FilesystemTest.php
@@ -64,7 +64,64 @@ class FilesystemTest extends TestCase {
         $template = new Test('test', 'route', false);
         $instance->setConfig(config('novapage.sources.' . $instance->getName()));
         $data = $instance->fetch($template);
-        $this->assertCount(1, $data['attributes']);
+        $this->assertCount(3, $data['attributes']);
         $this->assertSame('Test value', $data['attributes']['test_field']);
+    }
+
+    /** @test */
+    public function can_store_the_values_of_a_page()
+    {
+        $instance = new Filesystem();
+        $template = new Test('test', 'route', false);
+        $instance->setConfig(config('novapage.sources.' . $instance->getName()));
+        $template->load();
+        $this->assertNull($template->foobarbaz);
+        $template->foobarbaz = 'foobarbaz';
+        $instance->store($template);
+        $contents = json_decode(file_get_contents(__dir__ . '/../../test-application/resources/lang/route/test.json'));
+        $this->assertSame('foobarbaz', $contents->attributes->foobarbaz);
+
+        unset($contents->attributes->foobarbaz);
+        file_put_contents(__dir__ . '/../../test-application/resources/lang/route/test.json', json_encode($contents, JSON_PRETTY_PRINT, 512));
+    }
+
+    /** @test */
+    public function can_return_the_file_path()
+    {
+        $instance = new Filesystem();
+        $instance->setConfig(config('novapage.sources.' . $instance->getName()));
+        $this->assertSame(__dir__ . '/../../test-application/resources/lang/route/test.json', $instance->getFilePath('route', 'test'));
+    }
+
+    /** @test */
+    public function can_create_necessary_directories_when_storing_a_file()
+    {
+        $instance = new Filesystem();
+        $template = new Test('test', 'route', false);
+        $instance->setConfig(config('novapage.sources.' . $instance->getName()));
+        $template->load();
+
+        $this->rmrf(__dir__ . '/../../test-application/resources/lang/route');
+
+        $instance->store($template);
+        $this->assertDirectoryExists(__dir__ . '/../../test-application/resources/lang/route');
+        $this->assertFileExists(__dir__ . '/../../test-application/resources/lang/route/test.json');
+    }
+
+    /**
+     * Remove the directory and its content (all files and subdirectories).
+     * @param string $dir the directory name
+     */
+    private function rmrf($dir) {
+        if($dir == '' || $dir == '/') return;
+
+        foreach (glob($dir) as $file) {
+            if (is_dir($file)) {
+                $this->rmrf("$file/*");
+                rmdir($file);
+            } else {
+                unlink($file);
+            }
+        }
     }
 }
