84bd5c81eadc2bcc1e4adc7de361bbe6b90a8c11
thecaliskan <13554944+thecaliskan@users.noreply.github.com>
2021-08-30 14:09:03 +0300

    Detail Page 404
    #81

--- src/Http/Controllers/ActionController.php
+++ src/Http/Controllers/ActionController.php
@@ -3,7 +3,24 @@
 namespace Whitecube\NovaPage\Http\Controllers;
 
 use Laravel\Nova\Http\Controllers\ActionController as Controller;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 class ActionController extends Controller
 {
+    /**
+     * List the actions for the given resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(NovaRequest $request)
+    {
+        return response()->json([
+            'actions' => [],
+            'pivotActions' => [
+                'name' => $request->pivotName(),
+                'actions' => [],
+            ],
+        ]);
+    }
 }
