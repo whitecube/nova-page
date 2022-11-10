d7972292d52dfdbdaf88a623c3da38be003e98c7
Abdullah Ghanem <3bdullah.ghanem@gmail.com>
2022-11-10 13:57:56 +0200

    Update Query.php

--- src/Pages/Query.php
+++ src/Pages/Query.php
@@ -108,6 +108,18 @@ class Query
                 return $this->repository->load($type, $name, $this->locale, $throwOnMissing);
             });
     }
+    
+    
+    /**
+     * Mimic eloquent's Builder and execute the query
+     *
+     * @param bool $throwOnMissing
+     * @return Illuminate\Support\Collection
+     */
+    public function exists($throwOnMissing = false)
+    {
+        return true;
+    }
 
     /**
      * Checks if template should be included in results based
